869a55290025fbc8fd113484edf393b67e138d1d
XanderWP <alex.sikorskij@gmail.com>
2018-08-08 11:13:43 +0300

    improve performance and stability, 1 config for client and server

--- __resource.lua
+++ __resource.lua
@@ -1,13 +1,13 @@
---[[-----------------
-	vRP_doorsControl By XanderWP from Ukraine with <3
-------------------------]]--
 dependency "vrp"
 
-server_script '@vrp/lib/utils.lua'
-server_script 'server.lua'
 
-client_scripts{ 
-  "client.lua"
+client_scripts {
+	"client/Tunnel.lua",
+  "client/Proxy.lua",
+	"client.lua",
 }
 
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+server_scripts{
+	"@vrp/lib/utils.lua",
+	"server.lua"
+}
--- server.lua
+++ server.lua
@@ -1,62 +1,33 @@
---[[-----------------
-	vRP_doorsControl By XanderWP from Ukraine with <3
-------------------------]]--
-local Tunnel = module("vrp", "lib/Tunnel")
-local Proxy = module("vrp", "lib/Proxy")
-vRP = Proxy.getInterface("vRP")
-vRPclient = Tunnel.getInterface("vRP","doorsControl")
+local cfg = module("config")
 
+AddEventHandler("vRP:playerSpawn",function(user_id,source,first_spawn)
+  if first_spawn then
+    TriggerClientEvent('vrpdoors:load', cfg.list)
+  end
+end)
 
-doorList = {
-    [1] = {freeze = true, key = "key_lspd", permission="police.pc", name = "LSPD"},
-    [2] = {freeze = true, key = "key_lspd", permission="police.pc", name = "LSPD"},
-    [3] = {freeze = true, key = "key_pd_boss", permission="boss_lspd.pc", name = "Captain room"},
-    [4] = {freeze = true, key = "key_lspd", permission="police.pc", name = "LSPD"},
-    [5] = {freeze = true, key = "key_lspd", permission="police.pc", name = "LSPD"},
-    [6] = {freeze = true, key = "key_lspd", permission="police.pc", name = "LSPD"},
-    [7] = {freeze = true, key = "key_lspd", permission="police.pc", name = "LSPD"},
-    [8] = {freeze = true, key = "key_lspd", permission="police.pc", name = "LSPD"},
-    [9] = {freeze = true, key = "key_lspd", permission="police.pc", name = "LSPD"},
-    [10] = {freeze = true, key = "key_lspd", permission="police.pc", name = "LSPD"},
-    [11] = {freeze = true, key = "key_lspd", permission="police.pc", name = "LSPD"},
-    [12] = {freeze = true, key = "key_lspd", permission="police.pc", name = "LSPD"},
-    [13] = {freeze = true, key = "key_lspd", permission="police.pc", name = "LSPD"},
-    [14] = {freeze = true, key = "key_lspd", permission="police.pc", name = "LSPD"},
-    [15] = {freeze = true, key = "key_lspd", permission="police.pc", name = "LSPD"},
-    [16] = {freeze = true, key = "key_lspd", permission="police.pc", name = "LSPD"},
-	[17] = {freeze = true, key = "key_lspd", permission="police.pc", name = "LSPD"},
-	[18] = {freeze = true, key = "key_lspd", permission="police.pc", name = "LSPD"},
-	[19] = {freeze = true, key = "key_lspd", permission="police.pc", name = "LSPD"},
-	[20] = {freeze = true, key = "key_lspd", permission="police.pc", name = "LSPD"},
-	[21] = {freeze = true, key = "key_lspd", permission="police.pc", name = "LSPD"},
-	[22] = {freeze = true, key = "key_lspd", permission="police.pc", name = "LSPD"},
-}
 
 RegisterServerEvent('door:status')
-AddEventHandler('door:status', function(id, status)
-  user_id = vRP.getUserId({source})
-  player = vRP.getUserSource({user_id})
-  if vRP.hasPermission({user_id, "#"..doorList[id].key..".>0"}) or vRP.hasPermission({user_id,doorList[id].permission}) then
-    doorList[id].freeze=status
-    TriggerClientEvent('door:statusSend', -1, id, status)
-    if status then
-      vRPclient.pnotify(player, {"Door ~r~closed ~y~with ~b~Key "..doorList[id].name})
-    else
-      vRPclient.pnotify(player, {"Door ~g~open ~y~with ~b~Key "..doorList[id].name})
-    end
+AddEventHandler('door:status', function(id)
+  local user_id = vRP.getUserId({source})
+  local player = vRP.getUserSource({user_id})
+  if vRP.hasPermission({user_id, "#"..cfg.list[id].key..".>0"}) or vRP.hasPermission({user_id,cfg.list[id].permission}) then
+    vRPclient.playAnim(player, {false,{{"misscommon@locked_door", "lockeddoor_tryopen", 1}},false})
+    SetTimeout(4000, function()
+      cfg.list[id].locked = not cfg.list[id].locked
+      TriggerClientEvent('vrpdoors:statusSend', id,cfg.list[id].locked)
+      if cfg.list[id].pair ~= nil then
+        local idsecond = cfg.list[id].pair
+        cfg.list[idsecond].locked = cfg.list[id].locked
+        TriggerClientEvent('vrpdoors:statusSend', idsecond,cfg.list[id].locked)
+      end
+      if cfg.list[id].locked then
+        vRPclient.notify(player, {"Дверь закрыта с помощью Ключа "..cfg.list[id].name})
+      else
+        vRPclient.notify(player, {"Дверь открыта с помощью Ключа "..cfg.list[id].name})
+      end
+    end)
   else
-    vRPclient.pnotify(player, {"~r~Need ~b~Key "..doorList[id].name})
-  end 
+    vRPclient.notify(player, {"Отсутствует Ключ от дверей "..cfg.list[id].name})
+  end
 end)
-
-RegisterServerEvent('door:load')
-AddEventHandler('door:load', function(id, status)
-  TriggerClientEvent('door:loadSend', -1, doorList) 
-end)
-
-
-local function doorSync() 
-TriggerClientEvent('door:loadSend', -1, doorList) 
-  SetTimeout(5000, doorSync)
-end
-doorSync()
